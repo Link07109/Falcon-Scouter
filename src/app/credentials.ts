0e5ed3c872e1fab987864c9dcda6d83e536bbef1
rnyeyuvuri <50530104+rnyeyuvuri@users.noreply.github.com>
2019-06-11 16:16:22 -0400

    Add files via upload

---
+++ src/app/credentials.ts
@@ -0,0 +1,10 @@
+export const X_TBA_AUTH_KEY = 'LuZ6vmMGf9rDpZfXvo67urtAeNODH4rHU2a2mJ5HoGApBxA1tNOmEr4vBccYjmDJ';
+
+export const FIREBASE_CONFIG = {
+    apiKey: 'AIzaSyDt7ruHB7j5VgCFtONBAqnBh1Q-k2NjN8Y',
+    authDomain: 'falconscoutapp.firebaseapp.com',
+    databaseURL: 'https://falconscoutapp.firebaseio.com',
+    projectId: 'falconscoutapp',
+    storageBucket: 'falconscoutapp.appspot.com',
+    messagingSenderId: '1040377302545'
+};
